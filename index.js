2640eb1c075a3dac76322074feafc35e949fced3
iamfutureflash <xenode2000@gmail.com>
2022-10-27 11:37:38 +0530

    updated slides

---
+++ index.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    direction: "vertical",
+    spaceBetween: 50,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
